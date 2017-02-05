c96e6570cf65655d2c05b8f39fa8fe0cd85913ad
hungbie <a0112086@u.nus.edu>
2017-02-05 04:57:11 +0000

    update highlight js

--- public/js/highlightRow.js
+++ public/js/highlightRow.js
@@ -6,61 +6,72 @@ var row2 = [];
 var row3 = []; 
 var rowlast = [];
 for (var i = 0; i < rows; i++) {
-	cell = $('#ranktable tbody tr:eq(' + i + ') td').eq(12);
-	if (Number(cell.html()) >= first) {
-		if (Number(cell.html()) > first) {
-			row1 = [];
-			row1.push(i);
-		}
-		else {
-			row1.push(i);
-		}
-		first = Number(cell.html());
-	}
-	else if (Number(cell.html()) >= second) {
-		if (Number(cell.html()) > second) {
-			row2 = [];
-			row2.push(i);
-		}
-		else {
-			row2.push(i);
-		}
-		second = Number(cell.html());
-	}
-	else if (Number(cell.html()) >= third) {
-		if (Number(cell.html()) > third) {
-			row3 = [];
-			row3.push(i);
-		}
-		else {
-			row3.push(i);
-		}
-		third = Number(cell.html());
-	}
-	else if (Number(cell.html()) <= last) {
-		if (Number(cell.html()) < last) {
+	current = Number($('#ranktable tbody tr:eq(' + i + ') td').eq(12).html());
+	if (current <= last) {
+		if (current < last) {
 			rowlast = [];
 			rowlast.push(i);
 		}
 		else {
 			rowlast.push(i);
 		}
-		last = Number(cell.html());
+		last = current;
+	}
+	if (current >= first) {
+		if (current > first) {
+			if (first >= second) {
+				if (second >= third) {
+					third = second;
+					row3 = row2;
+				}
+				second = first;
+				row2 = row1;
+			}
+			row1 = [];
+			row1.push(i);
+		}
+		else {
+			row1.push(i);
+		}
+		first = current;
+	}
+	else if (current >= second) {
+		if (current > second) {
+			if (second >= third) {
+				third = second;
+				row3 = row2;
+			}
+			row2 = [];
+			row2.push(i);
+		}
+		else {
+			row2.push(i);
+		}
+		second = current;
+	}
+	else if (current >= third) {
+		if (current > third) {
+			row3 = [];
+			row3.push(i);
+		}
+		else {
+			row3.push(i);
+		}
+		third = current;
 	}
 	else {
 		continue;
 	}
 }
 for (var x = 0; x < rowlast.length; x++) {
-	$('#ranktable tbody tr').eq(rowlast[x]).css("backgroundColor", "pink");
+	$('#ranktable tbody tr').eq(rowlast[x]).addClass("pink");
 }
 for (x = 0; x < row3.length; x++) {
-	$('#ranktable tbody tr').eq(row3[x]).css("backgroundColor", "#cd7f32");
+	$('#ranktable tbody tr').eq(row3[x]).addClass("bronze");
 }
 for (x = 0; x < row2.length; x++) {
-	$('#ranktable tbody tr').eq(row2[x]).css("backgroundColor", "silver");
-}
+	$('#ranktable tbody tr').eq(row2[x]).addClass("silver");
 for (x = 0; x < row1.length; x++) {
-	$('#ranktable tbody tr').eq(row1[x]).css("backgroundColor", "gold");
+	$('#ranktable tbody tr').eq(row1[x]).addClass("gold");
+	}
 }
-

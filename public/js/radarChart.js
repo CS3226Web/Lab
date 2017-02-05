750dc5c55f57d7f08f51b6d21ab272d5e62da344
hungbie <a0112086@u.nus.edu>
2017-02-06 00:01:48 +0800

    Update radarChart.js

--- public/js/radarChart.js
+++ public/js/radarChart.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 //change these to variables
 var score = [];
-var maxScore = 80;
+var maxScore = 100;
 var length = maxScore; //max point
 
 $('#detail tbody tr').each(function(){
@@ -14,30 +14,30 @@ $('#detail tbody tr').each(function(){
 
 //Radar chart outline
 ctx.font = "15px Arial";
-ctx.fillText("MC",100+ Math.cos(Math.PI/6)*length + 2,100-Math.sin(Math.PI/6)*length - 2);
-ctx.fillText("TC",100 - 10,100-length - 2);
-ctx.fillText("HW",100-Math.cos(Math.PI/6)*length -22,100-Math.sin(Math.PI/6)*length -2);
-ctx.fillText("Bs",100-Math.cos(Math.PI/6)*length -22,100+Math.sin(Math.PI/6)*length +12);
-ctx.fillText("KS",100 - 10, 100 + length + 15);
-ctx.fillText("Ac",100+ Math.cos(Math.PI/6)*length + 2,100+Math.sin(Math.PI/6)*length + 2);
+ctx.fillText("MC",150+ Math.cos(Math.PI/6)*length + 2,150-Math.sin(Math.PI/6)*length - 2);
+ctx.fillText("TC",150 - 10,150-length - 2);
+ctx.fillText("HW",150-Math.cos(Math.PI/6)*length -22,150-Math.sin(Math.PI/6)*length -2);
+ctx.fillText("Bs",150-Math.cos(Math.PI/6)*length -22,150+Math.sin(Math.PI/6)*length +12);
+ctx.fillText("KS",150 - 10, 150 + length + 15);
+ctx.fillText("Ac",150+ Math.cos(Math.PI/6)*length + 2,150+Math.sin(Math.PI/6)*length + 2);
 drawOutline(1);
 drawOutline(0.75);
 drawOutline(0.5);
 drawOutline(0.25);
 function drawOutline(scale){
      var length = maxScore * scale;
-     drawLine(100,100,100+ Math.cos(Math.PI/6)*length,100-Math.sin(Math.PI/6)*length);
-     drawLine(100,100,100,100-length);
-     drawLine(100,100,100-Math.cos(Math.PI/6)*length,100-Math.sin(Math.PI/6)*length);
-     drawLine(100,100,100- Math.cos(Math.PI/6)*length,100+Math.sin(Math.PI/6)*length);
-     drawLine(100,100,100,100+length);
-     drawLine(100,100,100+ Math.cos(Math.PI/6)*length,100+Math.sin(Math.PI/6)*length);
-     drawLine(100+ Math.cos(Math.PI/6)*length,100-Math.sin(Math.PI/6)*length,100,100-length);
-     drawLine(100,100-length,100-Math.cos(Math.PI/6)*length,100-Math.sin(Math.PI/6)*length);
-     drawLine(100-Math.cos(Math.PI/6)*length,100-Math.sin(Math.PI/6)*length,100- Math.cos(Math.PI/6)*length,100+Math.sin(Math.PI/6)*length);
-     drawLine(100- Math.cos(Math.PI/6)*length,100+Math.sin(Math.PI/6)*length,100,100+length);
-     drawLine(100,100+length,100+ Math.cos(Math.PI/6)*length,100+Math.sin(Math.PI/6)*length);
-     drawLine(100+ Math.cos(Math.PI/6)*length,100+Math.sin(Math.PI/6)*length,100+ Math.cos(Math.PI/6)*length,100-Math.sin(Math.PI/6)*length);
+     drawLine(150,150,150+ Math.cos(Math.PI/6)*length,150-Math.sin(Math.PI/6)*length);
+     drawLine(150,150,150,150-length);
+     drawLine(150,150,150-Math.cos(Math.PI/6)*length,150-Math.sin(Math.PI/6)*length);
+     drawLine(150,150,150- Math.cos(Math.PI/6)*length,150+Math.sin(Math.PI/6)*length);
+     drawLine(150,150,150,150+length);
+     drawLine(150,150,150+ Math.cos(Math.PI/6)*length,150+Math.sin(Math.PI/6)*length);
+     drawLine(150+ Math.cos(Math.PI/6)*length,150-Math.sin(Math.PI/6)*length,150,150-length);
+     drawLine(150,150-length,150-Math.cos(Math.PI/6)*length,150-Math.sin(Math.PI/6)*length);
+     drawLine(150-Math.cos(Math.PI/6)*length,150-Math.sin(Math.PI/6)*length,150- Math.cos(Math.PI/6)*length,150+Math.sin(Math.PI/6)*length);
+     drawLine(150- Math.cos(Math.PI/6)*length,150+Math.sin(Math.PI/6)*length,150,150+length);
+     drawLine(150,150+length,150+ Math.cos(Math.PI/6)*length,150+Math.sin(Math.PI/6)*length);
+     drawLine(150+ Math.cos(Math.PI/6)*length,150+Math.sin(Math.PI/6)*length,150+ Math.cos(Math.PI/6)*length,150-Math.sin(Math.PI/6)*length);
 }
 function drawLine(startX, startY, endX, endY) {
     ctx.moveTo(startX,startY);
@@ -48,11 +48,11 @@ function drawLine(startX, startY, endX, endY) {
 //Radar Chart info
 ctx.fillStyle ='rgba(96,96,96,0.7)';
 ctx.beginPath();
-ctx.moveTo(100+ Math.cos(Math.PI/6)*score[0],100-Math.sin(Math.PI/6)*score[0]);
-ctx.lineTo(100,100 - score[1]);
-ctx.lineTo(100-Math.cos(Math.PI/6)*score[2],100-Math.sin(Math.PI/6)*score[2]);
-ctx.lineTo(100- Math.cos(Math.PI/6)*score[3],100+Math.sin(Math.PI/6)*score[3]);
-ctx.lineTo(100,100+score[4]);
-ctx.lineTo(100+ Math.cos(Math.PI/6)*score[5],100+Math.sin(Math.PI/6)*score[5]);
+ctx.moveTo(150+ Math.cos(Math.PI/6)*score[0],150-Math.sin(Math.PI/6)*score[0]);
+ctx.lineTo(150,150 - score[1]);
+ctx.lineTo(150-Math.cos(Math.PI/6)*score[2],150-Math.sin(Math.PI/6)*score[2]);
+ctx.lineTo(150- Math.cos(Math.PI/6)*score[3],150+Math.sin(Math.PI/6)*score[3]);
+ctx.lineTo(150,150+score[4]);
+ctx.lineTo(150+ Math.cos(Math.PI/6)*score[5],150+Math.sin(Math.PI/6)*score[5]);
 ctx.closePath();
 ctx.fill();
